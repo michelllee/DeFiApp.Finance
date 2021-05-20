91bb675ace302ef7b27e1673b902d10d03625222
Dagem Amare <dagemamare@gmail.com>
2021-05-21 00:52:12 +0300

    added speakers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   // Home
-  { path: '', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule), data: { breadcrumb: 'Homepage' } },
-  { path: 'home-v2', loadChildren: () => import('./components/pages/home-v2/home-v2.module').then(m => m.HomeV2Module), data: { breadcrumb: 'Homepage' } },
+  // { path: '', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule), data: { breadcrumb: 'Homepage' } },
+  { path: '', loadChildren: () => import('./components/pages/home-v2/home-v2.module').then(m => m.HomeV2Module), data: { breadcrumb: 'Homepage' } },
   { path: 'home-v3', loadChildren: () => import('./components/pages/home-v3/home-v3.module').then(m => m.HomeV3Module), data: { breadcrumb: 'Homepage' } },
   // About
   { path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule), data: { breadcrumb: 'About Us' } },
@@ -17,7 +17,7 @@ const routes: Routes = [
   // Pages
   { path: 'shop/cat/:catId', loadChildren: () => import('./components/pages/shop/shop.module').then(m => m.ShopModule), data: { breadcrumb: 'Shop' } },
   { path: 'shop/tag/:tagId', loadChildren: () => import('./components/pages/shop/shop.module').then(m => m.ShopModule), data: { breadcrumb: 'Shop' } },
-  
+
   { path: 'shop', loadChildren: () => import('./components/pages/shop/shop.module').then(m => m.ShopModule), data: { breadcrumb: 'Shop' } },
   { path: 'shop-details/:id', loadChildren: () => import('./components/pages/shop-details/shop-details.module').then(m => m.ShopDetailsModule), data: { breadcrumb: 'Shop Details' } },
   { path: 'shedules', loadChildren: () => import('./components/pages/schedules/schedules.module').then(m => m.SchedulesModule), data: { breadcrumb: 'Schedules' } },
@@ -33,7 +33,7 @@ const routes: Routes = [
   { path: 'blog/cat/:catId', loadChildren: () => import('./components/pages/blog-grid-two/blog-grid-two.module').then(m => m.BlogGridTwoModule), data: { breadcrumb: 'Blog Grid 3 Col' } },
   { path: 'blog/tag/:tagId', loadChildren: () => import('./components/pages/blog-grid-two/blog-grid-two.module').then(m => m.BlogGridTwoModule), data: { breadcrumb: 'Blog Grid 3 Col' } },
   { path: 'blog/author/:authorId', loadChildren: () => import('./components/pages/blog-grid-two/blog-grid-two.module').then(m => m.BlogGridTwoModule), data: { breadcrumb: 'Blog Grid 3 Col' } },
-  
+
   { path: 'blog-standard', loadChildren: () => import('./components/pages/blog-list/blog-list.module').then(m => m.BlogListModule), data: { breadcrumb: 'Blog Standard' } },
   { path: 'blog-grid', loadChildren: () => import('./components/pages/blog-grid/blog-grid.module').then(m => m.BlogGridModule), data: { breadcrumb: 'Blog Grid' } },
   { path: 'blog-grid-v2', loadChildren: () => import('./components/pages/blog-grid-two/blog-grid-two.module').then(m => m.BlogGridTwoModule), data: { breadcrumb: 'Blog Grid 3 Col' } },
